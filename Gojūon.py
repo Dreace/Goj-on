b29c7bbed9a3749ea6b650d923df54c614b134d6
Dreace <Dreace@Foxmail.com>
2021-07-25 22:54:38 +0800

    增加正确率显示

--- Gojūon.py
+++ Gojūon.py
@@ -18,7 +18,7 @@ def main():
         for katakana in line["katakana"]:
             print(katakana, end=' ')
         print()
-    print("\n1. ひらがな\t2.カタカナ\t3.すべて")
+    print("\n1. ひらがな  2.カタカナ  3.すべて")
     remember_type = int(input("モードを選択: "))
     input_text = input("\n行を選択: ")
     indexes = []
@@ -35,22 +35,28 @@ def main():
                 "katakana": data[index]["katakana"][i],
                 "pronunciation": data[index]["pronunciation"][i],
             })
-
+    total_count = 1e-10
+    correct_count = 0
     while True:
         cls()
+        print(f"正しいレート: {correct_count / total_count * 100:.2f}% ({correct_count}/{int(total_count)})")
+        print("\n")
         x = random.choice(candidates)
         kanas = [x["hiragana"], x["katakana"]]
+        print("仮名:",end=' ')
         if 1 <= remember_type <= 2:
-            print(kanas[remember_type - 1])
+            print(f"\033[91m{kanas[remember_type - 1]}\033[00m")
         else:
-            print(kanas[random.randint(0, 1)])
+            print(f"\033[91m{kanas[random.randint(0, 1)]}\033[00m")
         pronunciation = input("ローマ人: ")
         if pronunciation == x["pronunciation"]:
             print("✔")
+            correct_count += 1
         else:
             print("❌")
             print(f"({x['hiragana']} {x['katakana']} {x['pronunciation']})")
             input("何かキーを押すと続行します")
+        total_count += 1
 
 
 if __name__ == '__main__':
